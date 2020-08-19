65db03b0e4254be865b0f77c3859f419c20110f2
takeshi furusawa <djpdjp0517@gmail.com>
2020-08-19 13:52:51 +0000

    バリデーションを追加

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ApplicationRecord
+  validates :title, presence: true
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :comments, allow_destroy: true, reject_if: :all_blank
 end
